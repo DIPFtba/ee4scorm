c18661e6cb17d93d222413a65c85e9118c880c6c
fwagner <fwagner@dipf.de>
2023-08-15 18:10:17 +0200

    added suppport for SCORM 2004 3rd - build script

--- utils/build.sh
+++ utils/build.sh
@@ -19,7 +19,7 @@ build_test(){
 		
 		for WRAPPER_PATH in $( ls -d ./ee/SCORM_wrapper/*/ )
 		do
-			cd $WRAPPER_PATH && export SCORMPATH=${PWD##*/} && cd $BASEPATH
+			export SCORMPATH=${WRAPPER_PATH##*/}
 			export ZIPNAME="SCORM_${SCORMPATH}_${FOLDER}_$(date '+%d-%m-%Y-%H:%M')"
 			mkdir $ZIPNAME
 			cp -a ./ee/SCORM_wrapper/${SCORMPATH}/. ./${ZIPNAME}/
