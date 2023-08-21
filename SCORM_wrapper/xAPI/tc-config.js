8ffe8dc66c7e5611c36e91ae365d9ca28af9ac1f
fwagner <fwagner@dipf.de>
2023-08-21 17:14:56 +0200

    xAPI wrapper

---
+++ SCORM_wrapper/xAPI/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://xAPI_CBAItemBuilder"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "ItemBuilder - xAPI Example"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;em&gt;Replace these instructions with your own course content.&lt;/em&gt;&lt;/p&gt;&lt;p&gt;&lt;em&gt;&lt;br data-text=&#34;true&#34;&gt;&lt;/em&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Template instructions:&amp;nbsp;&lt;/strong&gt;This template was designed to give you a head start on creating training. It contains lessons you might want to cover in a &lt;em&gt;Phishing 101&lt;/em&gt; course. It includes relevant headings, images, and writing prompts to guide you and is largely populated with placeholder text. You can easily replace this content with your own text and media.&lt;/p&gt;&lt;p&gt;&lt;br data-text=&#34;true&#34;&gt;Use this space to write an overview of your course for learners and instruct them to click the &amp;quot;&lt;em&gt;Start Course&lt;/em&gt;&amp;quot; button to begin.&lt;/p&gt;"
+};
